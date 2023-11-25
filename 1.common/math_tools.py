a9034178209b56c629b76b3be9d7b2fb5a7d4669
renatokuipers <57039361+renatokuipers@users.noreply.github.com>
2023-11-25 05:41:53 +0100

    Update math_tools.py
    Added several mathematical functions and algorithms

--- 1.common/math_tools.py
+++ 1.common/math_tools.py
@@ -1,1 +1,60 @@
 # Common mathematical functions and algorithms
+
+import numpy as np
+
+def linear_interpolate(x1, y1, x2, y2, x):
+    """Linear interpolation between two points."""
+    return y1 + (y2 - y1) * ((x - x1) / (x2 - x1))
+
+def gaussian(x, mean, std):
+    """Gaussian function."""
+    return np.exp(-0.5 * ((x - mean) / std) ** 2) / (std * np.sqrt(2 * np.pi))
+
+def logistic(x, L=1, k=1, x0=0):
+    """Logistic function."""
+    return L / (1 + np.exp(-k * (x - x0)))
+
+def softmax(x):
+    """Softmax function."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def root_mean_square_error(y_true, y_pred):
+    """Calculate Root Mean Square Error."""
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+def euclidean_distance(point1, point2):
+    """Calculate Euclidean distance between two points."""
+    return np.linalg.norm(np.array(point1) - np.array(point2))
+
+def cosine_similarity(vec1, vec2):
+    """Calculate the cosine similarity between two vectors."""
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+def fourier_transform(signal):
+    """Compute the one-dimensional discrete Fourier Transform."""
+    return np.fft.fft(signal)
+
+def inverse_fourier_transform(signal):
+    """Compute the inverse one-dimensional discrete Fourier Transform."""
+    return np.fft.ifft(signal)
+
+def sigmoid(x):
+    """Sigmoid function."""
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    """Rectified Linear Unit (ReLU) function."""
+    return np.maximum(0, x)
+
+def normalize_data(data, lower=0, upper=1):
+    """Normalize data to a specific range."""
+    min_val = np.min(data)
+    max_val = np.max(data)
+    return (data - min_val) / (max_val - min_val) * (upper - lower) + lower
+
+def integrate_trapz(y, x):
+    """Trapezoidal integration."""
+    return np.trapz(y, x)
+
+# Add more mathematical functions and algorithms as needed

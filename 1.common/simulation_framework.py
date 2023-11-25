25d30b94bb239be22befc857a70462349fcfdc4a
renatokuipers <57039361+renatokuipers@users.noreply.github.com>
2023-11-25 05:43:46 +0100

    Update simulation_framework.py
    Added base classes and functions for setting up and running simulations

--- 1.common/simulation_framework.py
+++ 1.common/simulation_framework.py
@@ -1,1 +1,41 @@
 # Base classes and functions for setting up and running simulations
+
+import time
+
+class Simulation:
+    def __init__(self, parameters):
+        self.parameters = parameters
+        self.is_running = False
+        self.current_time = 0
+
+    def initialize(self):
+        """Initialize the simulation with the given parameters."""
+        raise NotImplementedError("Initialize method must be implemented.")
+
+    def step(self):
+        """Advance the simulation by one time step."""
+        raise NotImplementedError("Step method must be implemented.")
+
+    def run(self, duration):
+        """Run the simulation for a given duration."""
+        self.is_running = True
+        start_time = time.time()
+        while self.is_running and self.current_time < duration:
+            self.step()
+            self.current_time += 1
+        end_time = time.time()
+        print(f"Simulation completed in {end_time - start_time} seconds.")
+
+    def stop(self):
+        """Stop the simulation."""
+        self.is_running = False
+
+class NeuronSimulation(Simulation):
+    def initialize(self):
+        """Initialize neuron-specific simulation settings."""
+        print("Initializing Neuron Simulation with parameters:", self.parameters)
+
+    def step(self):
+        """Advance neuron simulation by one time step."""
+        # Implement neuron simulation logic here
+        pass
